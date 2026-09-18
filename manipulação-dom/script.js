52be53db218c6787771dfdad493be3f30190cfa8
ALISSON ALMEIDA GOMES <52759068897@info.local>
2026-09-18 15:01:54 -0300

    :rocket: feat: Finalizado manipulação de dom

--- manipulação-dom/script.js
+++ manipulação-dom/script.js
@@ -81,16 +81,59 @@
 
 //* EVENTO DE FORMULARIO (submit)
 
-const form = document.querySelector("form"); // pegando direto pela tag form
+// const form = document.querySelector("form"); // pegando direto pela tag form
 
 // O comportamento padrão do formulario é recarregar a página ao enviar o submit
-form.addEventListener("submit", (e) => {
+// form.addEventListener("submit", (e) => {
 
-    // impede o comportamento 
-    e.preventDefault();
+//     impede o comportamento 
+//     e.preventDefault();
 
-    const nome  = document.getElementById("nome").value 
+//     const nome  = document.getElementById("nome").value 
 
-    console.log(`Nome: ${nome}`);
+//     console.log(`Nome: ${nome}`);
 
+// })
+
+
+//* Criando elementos na pagina
+// const novoElemento = document.createElement("p"); // cria o elemento p
+// novoElemento.innerText = "Elemento novo criado."; // cria um texto no elemento
+
+// const form = document.getElementById("form");
+
+// // estamos adicionando um elemento filho (p) dentro do pai (form)
+// form.appendChild(novoElemento);
+
+//* criar um botao
+// const botao = document.createElement("button");
+// botao.innerText = "Excluir elemento";
+
+// form.appendChild(botao);
+
+// botao.addEventListener("click", (e) => {
+//      e.preventDefault();
+
+//      novoElemento.remove();
+
+//      // apagando direto do pai
+//      form.removeChild(novoElemento);
+// })
+
+//* Adicionando elementos em uma lista, a partir de um input
+const input = document.getElementById("input"); // input
+const botao = document.getElementById("add"); // button
+const lista = document.getElementById("lista"); // ul
+
+botao.addEventListener("click", () => {
+
+     const valorDigitado = input.value; // pega o valor que foi digitado na caixinha/input
+     
+     const li = document.createElement("li"); // criando <li>
+     li.innerText = valorDigitado;
+
+     // colocando o filho (li) dentro do pai (ul)
+     lista.appendChild(li);
+
+     input.value = "";
 })
